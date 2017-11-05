59e51a39b1c97fe4c9f867977302fcdbb160eca5
CH <chris@chrishouse.us>
2017-11-05 12:53:02 -0600

    Adding blanked out firebase config file.

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,11 @@
+export const FirebaseConfig = {
+    firebase: {
+      apiKey: '',
+      authDomain: '',
+      databaseURL: '',
+      projectId: '',
+      storageBucket: '',
+      messagingSenderId: ''
+    }
+  };
+  
