961d0ad5abe93fe40666a86b277b034bbaae1930
CH <chris@chrishouse.us>
2017-11-07 19:07:48 -0600

    Inserting data into firebase collection.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,22 +5,28 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
 import * as firebase from 'firebase/app';
 
+export interface Shirt { name: string; price: number; }
+export interface ShirtId extends Shirt { id: string; }
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  user: Observable<firebase.User>;
-  items: Observable<any[]>;
-  msgVal: string = '';
+  _db:AngularFirestore;
+  shirts:  Observable<any[]>;
 
   constructor(public afAuth: AngularFireAuth, db: AngularFirestore) {
     this.afAuth.auth.signInAnonymously();
-
-    this.user = this.afAuth.authState;
-    this.items = db.collection('items').valueChanges();
-
+    this.shirts = db.collection('tshirts').valueChanges();
+    this._db = db;  
   }
+
+  addTshirt(sName: string, dPrice: number){
+    let shirtsCollection = this._db.collection<Shirt>('tshirts');
+    shirtsCollection.add({ name: sName, price: dPrice });
+  }
+
 
 }
